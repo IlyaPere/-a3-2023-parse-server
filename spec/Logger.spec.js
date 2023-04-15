fbb8ba406aecf565185d6228a013e4b8731f195c
IlyaPerevyazko <84346711+IlyaPere@users.noreply.github.com>
2023-04-14 22:16:11 -0600

    addTransport

--- spec/Logger.spec.js
+++ spec/Logger.spec.js
@@ -1,5 +1,69 @@
-var logging = require('../src/Adapters/Logger/WinstonLogger');
-var winston = require('winston');
+// var logging = require('../src/Adapters/Logger/WinstonLogger');
+// var winston = require('winston');
+
+// class TestTransport extends winston.Transport {
+//   log(level, msg, meta, callback) {
+//     callback(null, true);
+//   }
+// }
+
+// describe('Logger', () => {
+//   it('should add transport', () => {
+//     const testTransport = new (TestTransport)({
+//       name: 'test'
+//     });
+//     spyOn(testTransport, 'log');
+//     logging.addTransport(testTransport);
+//     expect(Object.keys(logging.logger.transports).length).toBe(4);
+//     logging.logger.info('hi');
+//     expect(testTransport.log).toHaveBeenCalled();
+//     logging.removeTransport(testTransport);
+//     expect(Object.keys(logging.logger.transports).length).toBe(3);
+//   });
+
+//   it('should have files transports', (done) => {
+//     reconfigureServer().then(() => {
+//       let transports = logging.logger.transports;
+//       let transportKeys = Object.keys(transports);
+//       expect(transportKeys.length).toBe(3);
+//       done();
+//     });
+//   });
+
+//   it('should disable files logs', (done) => {
+//     reconfigureServer({
+//       logsFolder: null
+//     }).then(() => {
+//       let transports = logging.logger.transports;
+//       let transportKeys = Object.keys(transports);
+//       expect(transportKeys.length).toBe(1);
+//       done();
+//     });
+//   });
+
+//   it('should enable JSON logs', (done) => {
+//     // Force console transport
+//     reconfigureServer({
+//       logsFolder: null,
+//       jsonLogs: true,
+//       silent: false
+//     }).then(() => {
+//       let spy = spyOn(process.stdout, 'write');
+//       logging.logger.info('hi', {key: 'value'});
+//       expect(process.stdout.write).toHaveBeenCalled();
+//       var firstLog = process.stdout.write.calls.first().args[0];
+//       expect(firstLog).toEqual(JSON.stringify({key: 'value', level: 'info', message: 'hi' })+'\n');
+//       return reconfigureServer({
+//         jsonLogs: false
+//       });
+//     }).then(() => {
+//       done();
+//     });
+//   });
+// });
+
+const logging = require('../src/Adapters/Logger/WinstonLogger');
+const winston = require('winston');
 
 class TestTransport extends winston.Transport {
   log(level, msg, meta, callback) {
@@ -9,7 +73,7 @@ class TestTransport extends winston.Transport {
 
 describe('Logger', () => {
   it('should add transport', () => {
-    const testTransport = new (TestTransport)({
+    const testTransport = new TestTransport({
       name: 'test'
     });
     spyOn(testTransport, 'log');
@@ -21,21 +85,21 @@ describe('Logger', () => {
     expect(Object.keys(logging.logger.transports).length).toBe(3);
   });
 
-  it('should have files transports', (done) => {
-    reconfigureServer().then(() => {
-      let transports = logging.logger.transports;
-      let transportKeys = Object.keys(transports);
+  it('should have file transports', (done) => {
+    reconfigureServer().then(() => {
+      const transports = logging.logger.transports;
+      const transportKeys = Object.keys(transports);
       expect(transportKeys.length).toBe(3);
       done();
     });
   });
 
-  it('should disable files logs', (done) => {
+  it('should disable file logs', (done) => {
     reconfigureServer({
       logsFolder: null
-    }).then(() => {
-      let transports = logging.logger.transports;
-      let transportKeys = Object.keys(transports);
+    }).then(() => {
+      const transports = logging.logger.transports;
+      const transportKeys = Object.keys(transports);
       expect(transportKeys.length).toBe(1);
       done();
     });
@@ -47,16 +111,16 @@ describe('Logger', () => {
       logsFolder: null,
       jsonLogs: true,
       silent: false
-    }).then(() => {
-      let spy = spyOn(process.stdout, 'write');
+    }).then(() => {
+      const spy = spyOn(process.stdout, 'write');
       logging.logger.info('hi', {key: 'value'});
       expect(process.stdout.write).toHaveBeenCalled();
-      var firstLog = process.stdout.write.calls.first().args[0];
+      const firstLog = process.stdout.write.calls.first().args[0];
       expect(firstLog).toEqual(JSON.stringify({key: 'value', level: 'info', message: 'hi' })+'\n');
       return reconfigureServer({
         jsonLogs: false
       });
-    }).then(() => {
+    }).then(() => {
       done();
     });
   });
